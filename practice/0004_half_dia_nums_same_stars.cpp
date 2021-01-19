21aa146fd1d4995be44d068b5bbd03c464b75c79
anupamvamsi <anupam.ismav@gmail.com>
2021-01-19 19:49:41 +0530

    practice 1 refactors

--- practice/0004_half_dia_nums_same_stars.cpp
+++ practice/0004_half_dia_nums_same_stars.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
 
-void FirstHalf(int& count, int num_rows);
-void SecondHalf(int& count, int num_rows);
+void FirstHalf(int& number, int num_rows);
+void SecondHalf(int& number, int num_rows);
 
 int main() {
-  int number_of_rows, count = 1;
+  int number_of_rows, number = 1;
   std::cout << "Enter number of rows: ";
   std::cin >> number_of_rows;
 
-  FirstHalf(count, number_of_rows);
-  SecondHalf(count, number_of_rows);
+  FirstHalf(number, number_of_rows);
+  SecondHalf(number, number_of_rows);
 }
 
-void FirstHalf(int& count, int num_rows) {
+void FirstHalf(int& number, int num_rows) {
   for (int row = 0; row < num_rows; row++) {
     for (int col = 0; col <= row; col++) {
-      std::cout << count;
+      std::cout << number;
 
       if (col < row) {
         std::cout << "*";
       }
     }
-    count++;
+    number++;
     std::cout << std::endl;
   }
 }
 
-void SecondHalf(int& count, int num_rows) {
-  count--;
+void SecondHalf(int& number, int num_rows) {
+  number--;
 
   for (int row = num_rows; row > 0; row--) {
     for (int col = 0; col < row; col++) {
-      std::cout << count;
+      std::cout << number;
 
       if (col < row - 1) {
         std::cout << "*";
       }
     }
-    count--;
+    number--;
     std::cout << "\n";
   }
 }
--- practice/0005_half_dia_nums_stars.cpp
+++ practice/0005_half_dia_nums_stars.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 
-void FirstHalf(int& count, int number_of_rows);
-void SecondHalf(int& count, int number_of_rows);
+void FirstHalf(int& number, int number_of_rows);
+void SecondHalf(int& number, int number_of_rows);
 
 int main() {
   int number_of_rows;
   std::cout << "Enter number of rows 'N': ";
   std::cin >> number_of_rows;
 
-  int count = 1;  // pass by reference "&"
+  int number = 1;  // pass by reference "&"
 
-  FirstHalf(count, number_of_rows);
-  SecondHalf(count, number_of_rows);
+  FirstHalf(number, number_of_rows);
+  SecondHalf(number, number_of_rows);
 }
 
-void FirstHalf(int& count, int number_of_rows) {
+void FirstHalf(int& number, int number_of_rows) {
   for (int row = 0; row < number_of_rows; row++) {
     for (int col = 0; col <= row; col++) {
-      std::cout << count++;
+      std::cout << number++;
 
       if (col < row) {
         std::cout << "*";
@@ -27,18 +27,18 @@ void FirstHalf(int& count, int number_of_rows) {
   }
 }
 
-void SecondHalf(int& count, int number_of_rows) {
-  count = count - number_of_rows;
+void SecondHalf(int& number, int number_of_rows) {
+  number = number - number_of_rows;
 
   for (int row = number_of_rows; row > 0; row--) {
     for (int col = 0; col < row; col++) {
-      std::cout << count++;
+      std::cout << number++;
 
       if (col < row - 1) {
         std::cout << "*";
       }
     }
-    count = (count + 1) - 2 * row;
+    number = (number + 1) - 2 * row;
     std::cout << std::endl;
   }
 }
