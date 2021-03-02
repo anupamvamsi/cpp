668df2b224b7a12871f288e54e7eb2621654f0e0
anupamvamsi <anupam.ismav@gmail.com>
2021-03-02 19:37:34 +0530

    SDL 1: Textures, Renderers, Buffers [buggy code]

--- projects/particle_fire_sim/src/main.cpp
+++ projects/particle_fire_sim/src/main.cpp
@@ -12,18 +12,51 @@ int main(int argc, char** argv) {
   }
 
   // Window
-  SDL_Window* window = NULL;
-  window = SDL_CreateWindow("Particle Fire Explosion!", SDL_WINDOWPOS_CENTERED,
-                            SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,
-                            SDL_WINDOW_SHOWN);
+  SDL_Window* window = SDL_CreateWindow(
+      "Particle Fire Explosion!", SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
   if (window == NULL) {
     std::cout << "SDL Window unable to initialize. SDL Error: "
               << SDL_GetError() << std::endl;
   }
 
+  // Renderer
+  SDL_Renderer* renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+  if (renderer = NULL) {  // bug is here, doing assignment instead of "=="
+    std::cout << "Could not create the renderer." << std::endl;
+  }
+
+  // Texture
+  SDL_Texture* texture =
+      SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+                        SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+  if (texture = NULL) {  // bug is here, doing assignment instead of "=="
+    std::cout << "Could not create the texture." << std::endl;
+  }
+
+  // Array of screen pixels stored in a buffer (w x h)
+  Uint32* buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+  /*
+    ADD AN EXCEPTION FOR ERROR HANDLING
+    IN THE CASE MEMORY ALLOCATION
+    THROUGH "new" ISN'T WORKING.
+  */
+
+  // Set the memory of the buffer
+  memset(buffer, 0x128, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+  // Pitch = The number of bytes b/w rows of pixel data (w x sizeof(Uint32))
+  SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  SDL_RenderPresent(renderer);
+
+  bool quit = false;
+
   // Event
   SDL_Event event;
-  bool quit = false;
+
   while (!quit) {
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
@@ -32,6 +65,9 @@ int main(int argc, char** argv) {
     }
   }
 
+  delete[] buffer;
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyTexture(texture);
   SDL_DestroyWindow(window);
   SDL_Quit();
 
