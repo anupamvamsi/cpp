b10f6a9052121f0a2525d05d047c65f6efcabb9f
anupamvamsi <anupam.ismav@gmail.com>
2021-03-03 10:04:21 +0530

    SDL 5: Animating colors using sin/cos fns

--- projects/particle_fire_sim/src/main.cpp
+++ projects/particle_fire_sim/src/main.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 
+#include <cmath>
 #include <iostream>
 
 #include "Screen.hpp"
@@ -13,14 +14,17 @@ int main(int argc, char** argv) {
 
   while (true) {
     // Draw particles:
+    // Animate colors with sin/cos and SDL_GetTicks
+    int elapsed = SDL_GetTicks();
+    unsigned char red = (1 + cos(elapsed * 0.001)) * 128;
+    unsigned char green = (1 + sin(elapsed * 0.002)) * 128;
+    unsigned char blue = (1 + cos(elapsed * 0.003)) * 128;
+
     for (int y{0}; y < pfe::Screen::SCREEN_HEIGHT; y++) {
       for (int x{0}; x < pfe::Screen::SCREEN_WIDTH; x++) {
-        screen1.SetPixel(x, y, 34, 89, 22, 50);
+        screen1.SetPixel(x, y, red, green, blue, 50);
       }
     }
-
-    screen1.SetPixel(pfe::Screen::SCREEN_WIDTH / 2,
-                     pfe::Screen::SCREEN_HEIGHT / 2, 255, 255, 255, 255);
 
     // Draw and update the screen:
     screen1.UpdateScreen();
