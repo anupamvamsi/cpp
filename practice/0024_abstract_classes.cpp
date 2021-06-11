052a2479ee8ccc19394e1054ab71c617563e716a
anupamvamsi <anupam.ismav@gmail.com>
2021-06-11 15:55:55 +0530

    Extra notes about 'virtual' and 'final' keywords

--- practice/0024_abstract_classes.cpp
+++ practice/0024_abstract_classes.cpp
@@ -7,15 +7,20 @@ class Book {
   std::string m_author;
 
  public:
-  Book(std::string title, std::string author) {
-    m_title = title;
-    m_author = author;
-  }
+  Book(std::string title, std::string author)
+      : m_title{title}, m_author{author} {}
 
   virtual void Display() = 0;  // pure virtual function declaration
+
+  // https://www.learncpp.com/cpp-tutorial/virtual-destructors-virtual-assignment-and-overriding-virtualization/
+  // “A base class destructor should be either public and virtual, or protected
+  // and nonvirtual.”
+ protected:
+  ~Book() {}
 };
 
-class MyBook : public Book {
+class MyBook final : public Book {  // declared as final to prevent further
+                                    // inheritance from "MyBook" class
  private:
   int m_price{0};
 
@@ -23,11 +28,23 @@ class MyBook : public Book {
   MyBook(const std::string& title, const std::string author, int price)
       : Book(title, author), m_price{price} {}
 
-  void Display() override {
+  // Doesn't need "virtual" specifier, but it is good practice to indicate it
+  virtual void Display() {
     std::cout << "Title: " << this->m_title << "\n";
     std::cout << "Author: " << this->m_author << "\n";
     std::cout << "Price: " << this->m_price << "\n";
   }
+
+  // IF the "final" specifier is not declared for the derived class (MyBook) OR,
+  // IF the dtor of the base class (Book) is "protected",
+  // The dtor of the derived class (MyBook) has to be "virtual", and it cannot
+  // be "protected", even if some other class is deriving from "MyBook" class.
+  // This means:
+  // >>> Base class dtor should be... ...or "protected and non-virtual" does not
+  // >>> apply here
+  //  protected:     // this will not work.
+  virtual ~MyBook() {
+  }  // virtual will work, if "final" is not declared for "MyBook"
 };
 
 int main() {
