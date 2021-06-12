a760fd7cf941a9f6853ef92c59b12bd91c26d1bb
anupamvamsi <anupam.ismav@gmail.com>
2021-06-12 18:02:16 +0530

    20.2: cerr, flow of program

--- tut_files/0032_advanced/0032_exceptions.cpp
+++ tut_files/0032_advanced/0032_exceptions.cpp
@@ -1,3 +1,5 @@
+// https://www.learncpp.com/cpp-tutorial/basic-exception-handling/
+
 #include <iostream>
 #include <string>
 
@@ -10,16 +12,17 @@ void errorDetector() {
     throw 42;  // function errorDetector() is aborted at the first throw
   }
 
-  if (error) {
+  if (!error) {
     throw "Error: Manually generated.";
   }
 
-  if (error) {
+  if (!error) {
     throw std::string{"Error: Generated with std::string."};
   }
 
-  if (!error) {
+  if (error) {
     throw 24.2890152219380724001629;
+    std::cout << "This line will never be printed.\n";
   }
 
   if (!error) {
@@ -35,27 +38,44 @@ int main() {
   try {
     errorDetector();
   } catch (int e) {
-    std::cout << e << " Integer error.\n";
+    std::cerr << e << " Integer error.\n";
   } catch (const char* e) {
-    std::cout << e << " char const* error.\n";
+    std::cerr << e << " char const* error.\n";
   }
 
   // Exceptions of non-fundamental types (like a class) should be caught by
-  // <const reference> to avoid making an unnecessary copy
+  // 'const' reference to avoid making an unnecessary copy
   catch (const std::string& e) {
-    std::cout << e << " std::string error.\n";
-  } catch (double e) {
-    std::cout.precision(50);
-    std::cout << std::fixed << e << " Double error.\n";
-  } catch (char e) {
-    std::cout << e << " - Char error.\n";
+    std::cerr << e << " std::string error.\n";
+  }
+
+  /* If the thrown exception is not caught (in this case, the double type), and
+   * if no 'catch' blocks exist to catch the exception, it will look for another
+   * 'try' block which can handle the exception. If none exist before the end of
+   * the program, then the program will terminate with the "Aborted (cored
+   * dumped)" error.
+   *
+   * Also, the compiler will not perform implicit conversions or promotions when
+   * matching exceptions with catch blocks. 'int' will not be matched with a
+   * 'char' block, and only casts from a derived class to one of the parent
+   * classes will be done if needed.
+   */
+  // catch (double e) {
+  //   std::cerr.precision(50);
+  //   std::cerr << std::fixed << e << " Double error.\n";
+  // }
+
+  catch (char e) {
+    std::cerr << e << " - Char error.\n";
   }
 
   // If exception parameter is not going to be used in the catch block, no
   // need to include the variable name
   catch (bool) {
-    std::cout << "Bool error.\n";
+    std::cerr << "Bool error.\n";
   }
+
+  std::cout << "Program is continuing...\n";
 
   return 0;
 }
