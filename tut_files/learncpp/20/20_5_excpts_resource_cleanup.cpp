6d319d30edcde0d6f06ad84b16a058b0025f3e87
anupamvamsi <anupam.ismav@gmail.com>
2021-06-18 02:59:16 +0530

    20.5: Fixed Ctor failure, added memory leak code for 'new' (heap) allocation

--- tut_files/learncpp/20/20_5_excpts_resource_cleanup.cpp
+++ tut_files/learncpp/20/20_5_excpts_resource_cleanup.cpp
@@ -13,14 +13,25 @@ class Member {
 class X {
  private:
   int m_x{};
-  Member m_member{};
+  // Memory leaks here: refs:
+  // https://stackoverflow.com/questions/147572/will-the-below-code-cause-memory-leak-in-c
+  // https://stackoverflow.com/questions/737653/whats-the-best-technique-for-exiting-from-a-constructor-on-an-error-condition-i
+  Member* m_member = new Member();  // members inited with 'new' not destroyed
+                                    // on failure of construction, memory leaks
+  Member m_mem{};
 
  public:
-  X(int x) : m_x{x} {
+  X(int x) : m_x{x} {  // default ctor
     if (x <= 0) {
+      // if (m_member == nullptr) {
+      std::cerr << m_member << '\n';
+      // }
       throw 1;
     }
   }
+
+  X(const X&) = delete;
+  X& operator=(const X&) = delete;
 
   ~X() { std::cerr << "~X\n"; }
 };
@@ -35,9 +46,15 @@ struct A {
 // Errors: rule of 0/3/5 being violated:
 // https://en.cppreference.com/w/cpp/language/rule_of_three
 struct B {
-  B() {}
   int* first = nullptr;
   std::vector<int> second{};
+
+  B() : first{nullptr}, second{} {}  // default ctor
+  B(const B&) = delete;              // copy ctor
+  B(B&&) = delete;                   // move ctor
+  B& operator=(const B&) = delete;   // copy assignment
+  B& operator=(B&&) = delete;        // move assignment
+  ~B() { delete first; }             // dtor
 };
 
 // Potential problems:
@@ -45,8 +62,15 @@ struct B {
 // because of 'C()' not being specified, and the reference member 'second' also
 // implicitly helps delete the assignment operator 'operator=(const B&)' of the
 // struct.
+std::vector<int> init_vec;
 struct C {
-  int* first;
+  C()
+      : second{init_vec}  // inits the 'second' vector<int> reference
+  {
+    //
+  }
+
+  int* first = nullptr;
   std::vector<int>& second;
 };
 
@@ -57,8 +81,10 @@ int main() {
     std::cerr << "Oops\n";
   }
 
-  // C c1;
-  // C c2;
+  // Errors now fixed
+  C c1;
+  C c2(c1);
+
   // c1 = c2;
 
   return 0;
